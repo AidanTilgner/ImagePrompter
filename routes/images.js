2054ee7200a424298078f398a07445b83891b8a9
Aidan Tilgner <aidantilgner02@gmail.com>
2022-11-04 23:24:32 -0700

    added a bunch of new stuff

--- routes/images.js
+++ routes/images.js
@@ -6,12 +6,21 @@ const router = Router();
 router.post("/", async (req, res) => {
   const { prompt, number, size } = req.body;
 
-  const { url, openai_url } = await generateImage(prompt, number, size);
+  const newImage = await generateImage(prompt, number, size);
+
+  if (!newImage) {
+    return res
+      .status(500)
+      .json({ message: "Error generating image", status: 500 });
+  }
+
+  const { url, openai_url } = newImage;
 
   res.send({
     message: "Image generated successfully!",
     url: url,
     openai_url: openai_url,
+    status: 200,
   });
 });
 
